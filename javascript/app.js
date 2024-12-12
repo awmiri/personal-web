41f399c7b3c668fbbb5826375c1c45b94ba019c7
awmiri <talasaz1382@gmail.com>
2024-12-12 14:58:43 +0330

    done style and after that create a effect for the title whit js

--- javascript/app.js
+++ javascript/app.js
@@ -15,5 +15,8 @@ getListItems.forEach(function (getListItem){
     getListItem.addEventListener('click' , function() {
         $.querySelector('.resume_title--item_active').classList.remove('resume_title--item_active')
         this.classList.add('resume_title--item_active')
+        $.querySelector('.resume__content--item_show').classList.remove('resume__content--item_show')
+        let checkClass = this.getAttribute('content-id')
+        $.querySelector(checkClass).classList.add("resume__content--item_show")
     })
 })
